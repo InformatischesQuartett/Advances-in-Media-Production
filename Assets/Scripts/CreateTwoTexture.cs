7200c2044d5e5bd684cb98c72e738bc820baebd6
Linda Schey <lindaschey@t-online.de>
2015-01-15 17:03:38 +0100

    fixed FramePackage

--- Assets/Scripts/CreateTwoTexture.cs
+++ Assets/Scripts/CreateTwoTexture.cs
@@ -7,6 +7,8 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
 
+using System.Diagnostics;
+
 public class CreateTwoTexture : MonoBehaviour
 {
     private AVProLiveCamera _liveCamera;
@@ -17,6 +19,11 @@ public class CreateTwoTexture : MonoBehaviour
 
     public Texture2D Right { get; private set; }
     public Texture2D Complete;
+
+	private Rect RectL, RectR, RectC;
+	private byte[] linData;
+	private bool first = true;
+	private Stopwatch stopwatch;
     public enum StereoFormat
     {
         FramePacking,
@@ -26,15 +33,21 @@ public class CreateTwoTexture : MonoBehaviour
 	// Use this for initialization
 	IEnumerator Start ()
 	{
+		stopwatch = new Stopwatch ();
         Left = Right = null;
 	    _liveCamera = this.GetComponent<AVProLiveCamera>();
 	    yield return new WaitForSeconds(1);
         CreateNewTexture(_liveCamera.OutputTexture, Format);
+
+
+		RenderTexture.active = (RenderTexture) _liveCamera.OutputTexture;
+		Complete.ReadPixels(new Rect(0, 0, Complete.width, Complete.height), 0, 0, false);
+		//Complete.Apply();
+		RenderTexture.active = null;
 	}
 
     private void OnGUI()
     {
-
         if (_liveCamera.OutputTexture != null && Left != null && Right != null)
         {
             GUI.DrawTexture(new Rect(250, 0, 200, 200), _liveCamera.OutputTexture, ScaleMode.ScaleToFit, false);
@@ -44,8 +57,7 @@ public class CreateTwoTexture : MonoBehaviour
     }
 
     void OnPostRender()
-    {
-            
+    {            
         if (_liveCamera.OutputTexture != null)
         {
             Convert();
@@ -73,10 +85,10 @@ public class CreateTwoTexture : MonoBehaviour
 
             RenderTexture.active = (RenderTexture)liveCamTexture;
 
-            Left.ReadPixels(new Rect(0, 0, Left.width, Left.height), 0, 0, false);
+            Left.ReadPixels(RectL, 0, 0, false);
             Left.Apply();
 
-            Right.ReadPixels(new Rect(Right.width, 0, Right.width*2, Right.height), 0, 0, false);
+            Right.ReadPixels(RectR, 0, 0, false);
             Right.Apply();
 
             RenderTexture.active = null;
@@ -86,11 +98,16 @@ public class CreateTwoTexture : MonoBehaviour
             CreateNewTexture(liveCamTexture, Format);
         }
     }
-
-    private byte[] PrepareRenderImage(Image<Rgba, byte> img)
+	 
+	private byte[] PrepareRenderImage(Image<Rgba, byte> img)
     {
         var convertedImg = img.Convert<Rgb, byte>();
-        var linData = new byte[convertedImg.Data.Length];
+		if (first == true) 
+		{
+			linData = new byte[convertedImg.Data.Length];
+			first= false;
+		}
+
         Buffer.BlockCopy(convertedImg.Data, 0, linData, 0, convertedImg.Data.Length);
         return linData;
     }
@@ -125,26 +142,34 @@ public class CreateTwoTexture : MonoBehaviour
         if (Complete != null)
         {
             RenderTexture.active = (RenderTexture) liveCamTexture;
-            Complete.ReadPixels(new Rect(0, 0, Complete.width, Complete.height), 0, 0, false);
-            Complete.Apply();
+			Complete.ReadPixels(RectC, 0, 0, false);
+            //Complete.Apply();
             RenderTexture.active = null;
+			stopwatch.Start();
             byte[] bytes = Color32ArrayToByteArray(Complete.GetPixels32());
+			stopwatch.Stop();
+			UnityEngine.Debug.Log("Coloe32ToByteArray"+stopwatch.Elapsed);
             fixed (byte* dataPtr = bytes)
             {
+				stopwatch.Start();
                 var depthImg = new Image<Rgba, byte>(liveCamTexture.width,liveCamTexture.height, 4*liveCamTexture.width,
                     new IntPtr(dataPtr));
-                var resizedImage = depthImg.Resize(liveCamTexture.width, liveCamTexture.height/2, INTER.CV_INTER_LINEAR,
+                var resizedImage = depthImg.Resize(liveCamTexture.width, liveCamTexture.height/2, INTER.CV_INTER_NN,
                     false);
                 depthImg = depthImg.Flip(FLIP.VERTICAL);
-                var resizedImage2 = depthImg.Resize(depthImg.Width, depthImg.Height / 2, INTER.CV_INTER_LINEAR,
+                var resizedImage2 = depthImg.Resize(depthImg.Width, depthImg.Height / 2, INTER.CV_INTER_NN,
                     false).Flip(FLIP.VERTICAL);
+				stopwatch.Stop();
+				UnityEngine.Debug.Log("CreatedepthImage"+stopwatch.Elapsed);
 
-                Left.LoadRawTextureData(PrepareRenderImage(resizedImage));
-                Left.Apply();
-                Right.LoadRawTextureData(PrepareRenderImage(resizedImage2));
+				stopwatch.Start();
+				Right.LoadRawTextureData(PrepareRenderImage(resizedImage));
                 Right.Apply();
-            }
-            
+                Left.LoadRawTextureData(PrepareRenderImage(resizedImage2));
+                Left.Apply();
+				stopwatch.Stop();
+				UnityEngine.Debug.Log("LoadedRawTexture"+stopwatch.Elapsed);
+            }            
         }
         else
         {
@@ -154,20 +179,20 @@ public class CreateTwoTexture : MonoBehaviour
 
     private void CreateNewTexture(Texture liveCamTexture, StereoFormat format)
     {
-
-
-
-        Debug.Log("CreateNewTexture");
+//        Debug.Log("CreateNewTexture");
         if (format == StereoFormat.SideBySide)
         {
             Left = new Texture2D(liveCamTexture.width / 2, liveCamTexture.height, TextureFormat.RGB24, false);
             Right = new Texture2D(liveCamTexture.width / 2, liveCamTexture.height, TextureFormat.RGB24, false);
+			RectL = new Rect(0, 0, Left.width, Left.height);
+			RectR = new Rect(Right.width, 0, Right.width*2, Right.height);
         }
         else if (format == StereoFormat.FramePacking)
         {
             Left = new Texture2D(liveCamTexture.width, liveCamTexture.height / 2, TextureFormat.RGB24, false);
             Right = new Texture2D(liveCamTexture.width, liveCamTexture.height / 2, TextureFormat.RGB24, false);
             Complete = new Texture2D(liveCamTexture.width, liveCamTexture.height, TextureFormat.RGB24, false);
+			RectC = new Rect(0, 0, Complete.width, Complete.height);
         }
     }
 }
