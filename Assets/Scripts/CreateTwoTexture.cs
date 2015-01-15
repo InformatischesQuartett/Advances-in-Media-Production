45c29f212a75652fad1ea897097397c3a2bbb751
Linda Schey <lindaschey@t-online.de>
2015-01-15 17:11:13 +0100

    cleand up some stuff

--- Assets/Scripts/CreateTwoTexture.cs
+++ Assets/Scripts/CreateTwoTexture.cs
@@ -7,7 +7,6 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
 
-using System.Diagnostics;
 
 public class CreateTwoTexture : MonoBehaviour
 {
@@ -20,10 +19,11 @@ public class CreateTwoTexture : MonoBehaviour
     public Texture2D Right { get; private set; }
     public Texture2D Complete;
 
-	private Rect RectL, RectR, RectC;
-	private byte[] linData;
-	private bool first = true;
-	private Stopwatch stopwatch;
+	private Rect _rectL, _rectR, _rectC;
+
+	private byte[] _linData;
+	private bool _first = true;
+
     public enum StereoFormat
     {
         FramePacking,
@@ -33,16 +33,15 @@ public class CreateTwoTexture : MonoBehaviour
 	// Use this for initialization
 	IEnumerator Start ()
 	{
-		stopwatch = new Stopwatch ();
         Left = Right = null;
 	    _liveCamera = this.GetComponent<AVProLiveCamera>();
-	    yield return new WaitForSeconds(1);
-        CreateNewTexture(_liveCamera.OutputTexture, Format);
 
+	    yield return new WaitForSeconds(1);
+
+        CreateNewTexture(_liveCamera.OutputTexture, Format);
 
 		RenderTexture.active = (RenderTexture) _liveCamera.OutputTexture;
 		Complete.ReadPixels(new Rect(0, 0, Complete.width, Complete.height), 0, 0, false);
-		//Complete.Apply();
 		RenderTexture.active = null;
 	}
 
@@ -85,10 +84,10 @@ public class CreateTwoTexture : MonoBehaviour
 
             RenderTexture.active = (RenderTexture)liveCamTexture;
 
-            Left.ReadPixels(RectL, 0, 0, false);
+            Left.ReadPixels(_rectL, 0, 0, false);
             Left.Apply();
 
-            Right.ReadPixels(RectR, 0, 0, false);
+            Right.ReadPixels(_rectR, 0, 0, false);
             Right.Apply();
 
             RenderTexture.active = null;
@@ -102,14 +101,14 @@ public class CreateTwoTexture : MonoBehaviour
 	private byte[] PrepareRenderImage(Image<Rgba, byte> img)
     {
         var convertedImg = img.Convert<Rgb, byte>();
-		if (first == true) 
+		if (_first == true) 
 		{
-			linData = new byte[convertedImg.Data.Length];
-			first= false;
+			_linData = new byte[convertedImg.Data.Length];
+			_first= false;
 		}
 
-        Buffer.BlockCopy(convertedImg.Data, 0, linData, 0, convertedImg.Data.Length);
-        return linData;
+        Buffer.BlockCopy(convertedImg.Data, 0, _linData, 0, convertedImg.Data.Length);
+        return _linData;
     }
 
     private static byte[] Color32ArrayToByteArray(Color32[] colors)
@@ -142,16 +141,13 @@ public class CreateTwoTexture : MonoBehaviour
         if (Complete != null)
         {
             RenderTexture.active = (RenderTexture) liveCamTexture;
-			Complete.ReadPixels(RectC, 0, 0, false);
-            //Complete.Apply();
+			Complete.ReadPixels(_rectC, 0, 0, false);
             RenderTexture.active = null;
-			stopwatch.Start();
+
             byte[] bytes = Color32ArrayToByteArray(Complete.GetPixels32());
-			stopwatch.Stop();
-			UnityEngine.Debug.Log("Coloe32ToByteArray"+stopwatch.Elapsed);
             fixed (byte* dataPtr = bytes)
             {
-				stopwatch.Start();
+
                 var depthImg = new Image<Rgba, byte>(liveCamTexture.width,liveCamTexture.height, 4*liveCamTexture.width,
                     new IntPtr(dataPtr));
                 var resizedImage = depthImg.Resize(liveCamTexture.width, liveCamTexture.height/2, INTER.CV_INTER_NN,
@@ -159,16 +155,11 @@ public class CreateTwoTexture : MonoBehaviour
                 depthImg = depthImg.Flip(FLIP.VERTICAL);
                 var resizedImage2 = depthImg.Resize(depthImg.Width, depthImg.Height / 2, INTER.CV_INTER_NN,
                     false).Flip(FLIP.VERTICAL);
-				stopwatch.Stop();
-				UnityEngine.Debug.Log("CreatedepthImage"+stopwatch.Elapsed);
 
-				stopwatch.Start();
 				Right.LoadRawTextureData(PrepareRenderImage(resizedImage));
                 Right.Apply();
                 Left.LoadRawTextureData(PrepareRenderImage(resizedImage2));
                 Left.Apply();
-				stopwatch.Stop();
-				UnityEngine.Debug.Log("LoadedRawTexture"+stopwatch.Elapsed);
             }            
         }
         else
@@ -179,20 +170,19 @@ public class CreateTwoTexture : MonoBehaviour
 
     private void CreateNewTexture(Texture liveCamTexture, StereoFormat format)
     {
-//        Debug.Log("CreateNewTexture");
         if (format == StereoFormat.SideBySide)
         {
             Left = new Texture2D(liveCamTexture.width / 2, liveCamTexture.height, TextureFormat.RGB24, false);
             Right = new Texture2D(liveCamTexture.width / 2, liveCamTexture.height, TextureFormat.RGB24, false);
-			RectL = new Rect(0, 0, Left.width, Left.height);
-			RectR = new Rect(Right.width, 0, Right.width*2, Right.height);
+			_rectL = new Rect(0, 0, Left.width, Left.height);
+			_rectR = new Rect(Right.width, 0, Right.width*2, Right.height);
         }
         else if (format == StereoFormat.FramePacking)
         {
             Left = new Texture2D(liveCamTexture.width, liveCamTexture.height / 2, TextureFormat.RGB24, false);
             Right = new Texture2D(liveCamTexture.width, liveCamTexture.height / 2, TextureFormat.RGB24, false);
             Complete = new Texture2D(liveCamTexture.width, liveCamTexture.height, TextureFormat.RGB24, false);
-			RectC = new Rect(0, 0, Complete.width, Complete.height);
+			_rectC = new Rect(0, 0, Complete.width, Complete.height);
         }
     }
 }
