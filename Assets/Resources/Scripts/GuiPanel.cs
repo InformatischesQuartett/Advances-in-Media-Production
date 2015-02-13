db1785a94bd59ca96562ca3abe7188b225094476
Linda Schey <lindaschey@t-online.de>
2015-02-13 15:01:44 +0100

    deisplaying information in Gui Panel, more info si to be read and shown

---
+++ Assets/Resources/Scripts/GuiPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenInfo
+{
+    public float Distance { get; set; }
+    public float ScreenSize { get; set; }
+}
+
+public class GuiPanel : MonoBehaviour
+{
+
+    private ScreenControlls _screens;
+    private Text _text;
+	// Use this for initialization
+	void Start ()
+	{
+	    _screens = FindObjectOfType<ScreenControlls>();
+	    _text = gameObject.GetComponentInChildren<Text>();
+        _text.text = "Screen distance: " + _screens.ScreenDistance+ "\nScreen size: " + _screens.ScreenSize+ "\n";
+	}
+
+    void Update ()
+    {
+        _text.text = "Screen distance: " + _screens.ScreenDistance + "\nScreen size: " + _screens.ScreenSize + "\n";
+    }
+}
