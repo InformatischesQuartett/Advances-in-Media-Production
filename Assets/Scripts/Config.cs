0da5452c7073480b8eaa54a779af37feeea4dae0
Alexander Scheurer <mail@aspepex.net>
2015-02-13 17:43:09 +0100

    Screens are now normalized according to the config

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -15,6 +15,9 @@ public static class Config {
     public static float HitDefault { get; private set; }
     public static float HitSensitivity { get; private set; }
 
+    public static Vector2 AspectRatio { get; private set; }
+    public static Vector3 AspectRatioNorm { get; private set; }
+
     public static bool Monoscopic { get; private set; }
 
     public static List<Color> Colors
@@ -42,6 +45,24 @@ public static class Config {
         ScreenSizeSensitivity = conf.ScreenSizeSensitivity;
         HitDefault = conf.HitDefault;
         HitSensitivity = conf.HitSensitivity;
+
+        AspectRatio = new Vector2(conf.AspectRatioX, conf.AspectRatioY);
+        AspectRatioNorm = new Vector3();
+
+        if (conf.AspectRatioNorm == "horizontal")
+        {
+            AspectRatioNorm = new Vector3(1, 0, conf.AspectRatioY/conf.AspectRatioX);
+        }
+        else if (conf.AspectRatioNorm == "vertical")
+        {
+            AspectRatioNorm = new Vector3(conf.AspectRatioX/conf.AspectRatioY, 0, 1);
+        }
+        else
+        {
+            Debug.LogError("Config for AspectRatioNorm not valid! Valid options are 'horizontal' or 'vertical'.");
+            Application.Quit();
+        }
+
 
         Monoscopic = conf.Monoscopic;
         OVRManager.instance.monoscopic = Monoscopic;
@@ -73,6 +94,11 @@ public static class Config {
         public float ScreenSizeSensitivity;
         public float HitDefault;
         public float HitSensitivity;
+
+        public float AspectRatioX;
+        public float AspectRatioY;
+
+        public string AspectRatioNorm;
 
         public bool Monoscopic;
     }
--- Assets/Scripts/ScreenControlls.cs
+++ Assets/Scripts/ScreenControlls.cs
@@ -15,7 +15,6 @@ public class ScreenControlls : MonoBehaviour
     private Vector3 _positionVectorScreenR;
 
     private Vector3 _positionVector;
-    private Vector3 _aspectRatio;
 
     private List<GameObject> _cameras;
 
@@ -31,9 +30,11 @@ public class ScreenControlls : MonoBehaviour
         _positionVectorScreenL = _screenL.transform.position;
         _positionVectorScreenR = _screenR.transform.position;
 
+        _screenL.transform.localScale = Config.AspectRatioNorm;
+        _screenR.transform.localScale = Config.AspectRatioNorm;
+        
         _positionVector = this.transform.position;
-        _aspectRatio = this.transform.localScale/100;
-        Debug.Log(this.transform.localScale/100);
+
         _cameras = new List<GameObject>();
         _avalavailableColors = Config.Colors;
 
@@ -54,21 +55,22 @@ public class ScreenControlls : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Input.anyKey)
+
+        //HIT
+        Hit += Input.GetAxis("HIT")*Time.deltaTime*Config.HitSensitivity;
+        Hit = Mathf.Clamp(Hit, -10, 10);
+
+        //Distance
+        ScreenDistance += Input.GetAxis("Screen Distance")*Time.deltaTime*Config.ScreenDistanceSensitivity;
+        ScreenDistance = Mathf.Clamp(ScreenDistance, 0, 1000);
+
+        //SIZE
+        ScreenSize += Input.GetAxis("Screen Size")*Time.deltaTime*Config.ScreenSizeSensitivity;
+        ScreenSize = Mathf.Clamp(ScreenSize, 0, 20);
+
+        //COLOR
+        if (Input.anyKeyDown)
         {
-            //HIT
-            Hit += Input.GetAxis("HIT") * Time.deltaTime * Config.HitSensitivity;
-            Hit = Mathf.Clamp(Hit, -10, 10);
-
-            //Distance
-            ScreenDistance += Input.GetAxis("Screen Distance") * Time.deltaTime * Config.ScreenDistanceSensitivity;
-            ScreenDistance = Mathf.Clamp(ScreenDistance, 0, 1000);
-
-            //SIZE
-            ScreenSize += Input.GetAxis("Screen Size") * Time.deltaTime * Config.ScreenSizeSensitivity;
-            ScreenSize = Mathf.Clamp(ScreenSize, 0, 20);
-
-            //COLOR
             if (Input.GetAxis("Color Select") > 0)
             {
                 if (_currentColorIndex == _avalavailableColors.Count - 1)
@@ -83,31 +85,33 @@ public class ScreenControlls : MonoBehaviour
                 else
                     _currentColorIndex--;
             }
-
-            _positionVectorScreenL.x = Hit/2f;
-            _positionVectorScreenL.y = 0;
-            _positionVectorScreenL.z = 0;
-            _screenL.transform.localPosition = _positionVectorScreenL;
-
-            _positionVectorScreenR.x = -Hit/2f;
-            _positionVectorScreenR.y = 0;
-            _positionVectorScreenR.z = 0;
-            _screenR.transform.localPosition = _positionVectorScreenR;
-
-            _positionVector.x = 0;
-            _positionVector.y = 0;
-            _positionVector.z = ScreenDistance;
-            this.transform.position = _positionVector;
-
-            this.transform.localScale = _aspectRatio*ScreenSize;
-
-            SetCamerasBackground(_cameras, _avalavailableColors[_currentColorIndex]);
-
-            ScreenInfo.UpdateScreenVaues(ScreenDistance, new Vector2(this.transform.localScale.x, this.transform.localScale.z), Hit);
-
-            if (Input.GetAxis("TrackerReset") > 0)
-                OVRManager.display.RecenterPose();
         }
+
+        _positionVectorScreenL.x = Hit/2f;
+        _positionVectorScreenL.y = 0;
+        _positionVectorScreenL.z = 0;
+        _screenL.transform.localPosition = _positionVectorScreenL;
+
+        _positionVectorScreenR.x = -Hit/2f;
+        _positionVectorScreenR.y = 0;
+        _positionVectorScreenR.z = 0;
+        _screenR.transform.localPosition = _positionVectorScreenR;
+
+        _positionVector.x = 0;
+        _positionVector.y = 0;
+        _positionVector.z = ScreenDistance;
+        this.transform.position = _positionVector;
+
+        this.transform.localScale = ScreenSize * Vector3.one;
+
+        SetCamerasBackground(_cameras, _avalavailableColors[_currentColorIndex]);
+
+        ScreenInfo.UpdateScreenVaues(ScreenDistance,
+            new Vector2(this.transform.localScale.x, this.transform.localScale.z), Hit);
+
+        if (Input.GetAxis("TrackerReset") > 0)
+            OVRManager.display.RecenterPose();
+
     }
 
     private void SetCamerasBackground(List<GameObject> cameras, Color color)
